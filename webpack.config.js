52909d58f75824be81a2104d43f6f2e8deb4f2b7
Yukari <mistelinn@gmail.com>
2020-03-11 21:48:17 +0300

    Add deps

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+
+module.exports = {
+    entry: './js/index.js',
+    devtool: 'inline-source-map',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ["file-loader"]
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        watchContentBase: true,
+        progress: true
+    }
+}
